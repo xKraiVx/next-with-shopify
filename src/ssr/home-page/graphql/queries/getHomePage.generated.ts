5b51eb59f4f80b552ef5a9c716548f9c9e0298c6
Andrii Kryvda <kryvdaandrei@gmail.com>
2025-04-14 02:52:48 +0200

    fixed generated requests, temporary off any error in linter

--- src/ssr/home-page/graphql/queries/getHomePage.generated.ts
+++ src/ssr/home-page/graphql/queries/getHomePage.generated.ts
@@ -1,20 +1,158 @@
-import * as Types from '../../../../graphql/graphql-generated-types/types';
+import * as Types from "../../../../graphql/graphql-generated-types/types";
 
-import { useQuery, UseQueryOptions } from '@tanstack/react-query';
-import { fetcher } from '@/graphql/fetcher';
-export type GetHomePageQueryVariables = Types.Exact<{ [key: string]: never; }>;
+import { useQuery, UseQueryOptions } from "@tanstack/react-query";
+import { fetcher } from "@/graphql/fetcher";
+export type GetHomePageQueryVariables = Types.Exact<{ [key: string]: never }>;
 
+export type GetHomePageQuery = {
+  __typename?: "QueryRoot";
+  products: {
+    __typename?: "ProductConnection";
+    edges: Array<{
+      __typename?: "ProductEdge";
+      node: {
+        __typename?: "Product";
+        title: string;
+        handle: string;
+        collections: {
+          __typename?: "CollectionConnection";
+          edges: Array<{
+            __typename?: "CollectionEdge";
+            node: { __typename?: "Collection"; title: string };
+          }>;
+        };
+        variants: {
+          __typename?: "ProductVariantConnection";
+          edges: Array<{
+            __typename?: "ProductVariantEdge";
+            node: {
+              __typename?: "ProductVariant";
+              id: string;
+              title: string;
+              availableForSale: boolean;
+              compareAtPrice?: {
+                __typename?: "MoneyV2";
+                amount: any;
+                currencyCode: Types.CurrencyCode;
+              } | null;
+              price: {
+                __typename?: "MoneyV2";
+                amount: any;
+                currencyCode: Types.CurrencyCode;
+              };
+              image?: {
+                __typename?: "Image";
+                id?: string | null;
+                altText?: string | null;
+                height?: number | null;
+                width?: number | null;
+                url: any;
+              } | null;
+              metafields: Array<{
+                __typename?: "Metafield";
+                key: string;
+                value: string;
+              } | null>;
+            };
+          }>;
+        };
+      };
+    }>;
+  };
+};
 
-export type GetHomePageQuery = { __typename?: 'QueryRoot', products: { __typename?: 'ProductConnection', edges: Array<{ __typename?: 'ProductEdge', node: { __typename?: 'Product', title: string, handle: string, collections: { __typename?: 'CollectionConnection', edges: Array<{ __typename?: 'CollectionEdge', node: { __typename?: 'Collection', title: string } }> }, variants: { __typename?: 'ProductVariantConnection', edges: Array<{ __typename?: 'ProductVariantEdge', node: { __typename?: 'ProductVariant', id: string, title: string, availableForSale: boolean, compareAtPrice?: { __typename?: 'MoneyV2', amount: any, currencyCode: Types.CurrencyCode } | null, price: { __typename?: 'MoneyV2', amount: any, currencyCode: Types.CurrencyCode }, image?: { __typename?: 'Image', id?: string | null, altText?: string | null, height?: number | null, width?: number | null, url: any } | null, metafields: Array<{ __typename?: 'Metafield', key: string, value: string } | null> } }> } } }> } };
+export type ProductCardFragment = {
+  __typename?: "Product";
+  title: string;
+  handle: string;
+  collections: {
+    __typename?: "CollectionConnection";
+    edges: Array<{
+      __typename?: "CollectionEdge";
+      node: { __typename?: "Collection"; title: string };
+    }>;
+  };
+  variants: {
+    __typename?: "ProductVariantConnection";
+    edges: Array<{
+      __typename?: "ProductVariantEdge";
+      node: {
+        __typename?: "ProductVariant";
+        id: string;
+        title: string;
+        availableForSale: boolean;
+        compareAtPrice?: {
+          __typename?: "MoneyV2";
+          amount: any;
+          currencyCode: Types.CurrencyCode;
+        } | null;
+        price: {
+          __typename?: "MoneyV2";
+          amount: any;
+          currencyCode: Types.CurrencyCode;
+        };
+        image?: {
+          __typename?: "Image";
+          id?: string | null;
+          altText?: string | null;
+          height?: number | null;
+          width?: number | null;
+          url: any;
+        } | null;
+        metafields: Array<{
+          __typename?: "Metafield";
+          key: string;
+          value: string;
+        } | null>;
+      };
+    }>;
+  };
+};
 
-export type ProductCardFragment = { __typename?: 'Product', title: string, handle: string, collections: { __typename?: 'CollectionConnection', edges: Array<{ __typename?: 'CollectionEdge', node: { __typename?: 'Collection', title: string } }> }, variants: { __typename?: 'ProductVariantConnection', edges: Array<{ __typename?: 'ProductVariantEdge', node: { __typename?: 'ProductVariant', id: string, title: string, availableForSale: boolean, compareAtPrice?: { __typename?: 'MoneyV2', amount: any, currencyCode: Types.CurrencyCode } | null, price: { __typename?: 'MoneyV2', amount: any, currencyCode: Types.CurrencyCode }, image?: { __typename?: 'Image', id?: string | null, altText?: string | null, height?: number | null, width?: number | null, url: any } | null, metafields: Array<{ __typename?: 'Metafield', key: string, value: string } | null> } }> } };
+export type VariantForProductCardFragment = {
+  __typename?: "ProductVariant";
+  id: string;
+  title: string;
+  availableForSale: boolean;
+  compareAtPrice?: {
+    __typename?: "MoneyV2";
+    amount: any;
+    currencyCode: Types.CurrencyCode;
+  } | null;
+  price: {
+    __typename?: "MoneyV2";
+    amount: any;
+    currencyCode: Types.CurrencyCode;
+  };
+  image?: {
+    __typename?: "Image";
+    id?: string | null;
+    altText?: string | null;
+    height?: number | null;
+    width?: number | null;
+    url: any;
+  } | null;
+  metafields: Array<{
+    __typename?: "Metafield";
+    key: string;
+    value: string;
+  } | null>;
+};
 
-export type VariantForProductCardFragment = { __typename?: 'ProductVariant', id: string, title: string, availableForSale: boolean, compareAtPrice?: { __typename?: 'MoneyV2', amount: any, currencyCode: Types.CurrencyCode } | null, price: { __typename?: 'MoneyV2', amount: any, currencyCode: Types.CurrencyCode }, image?: { __typename?: 'Image', id?: string | null, altText?: string | null, height?: number | null, width?: number | null, url: any } | null, metafields: Array<{ __typename?: 'Metafield', key: string, value: string } | null> };
+export type PriceForProductCardFragment = {
+  __typename?: "MoneyV2";
+  amount: any;
+  currencyCode: Types.CurrencyCode;
+};
 
-export type PriceForProductCardFragment = { __typename?: 'MoneyV2', amount: any, currencyCode: Types.CurrencyCode };
-
-export type ImageForProductCardFragment = { __typename?: 'Image', id?: string | null, altText?: string | null, height?: number | null, width?: number | null, url: any };
-
+export type ImageForProductCardFragment = {
+  __typename?: "Image";
+  id?: string | null;
+  altText?: string | null;
+  height?: number | null;
+  width?: number | null;
+  url: any;
+};
 
 export const PriceForProductCardFragmentDoc = `
     fragment PriceForProductCard on MoneyV2 {
@@ -86,18 +224,21 @@ export const GetHomePageDocument = `
 }
     ${ProductCardFragmentDoc}`;
 
-export const useGetHomePageQuery = <
-      TData = GetHomePageQuery,
-      TError = unknown
-    >(
-      variables?: GetHomePageQueryVariables,
-      options?: Omit<UseQueryOptions<GetHomePageQuery, TError, TData>, 'queryKey'> & { queryKey?: UseQueryOptions<GetHomePageQuery, TError, TData>['queryKey'] }
-    ) => {
-    
-    return useQuery<GetHomePageQuery, TError, TData>(
-      {
-    queryKey: variables === undefined ? ['GetHomePage'] : ['GetHomePage', variables],
-    queryFn: fetcher<GetHomePageQuery, GetHomePageQueryVariables>(GetHomePageDocument).bind(null, variables),
-    ...options
+export const useGetHomePageQuery = <TData = GetHomePageQuery, TError = unknown>(
+  variables?: GetHomePageQueryVariables,
+  options?: Omit<
+    UseQueryOptions<GetHomePageQuery, TError, TData>,
+    "queryKey"
+  > & {
+    queryKey?: UseQueryOptions<GetHomePageQuery, TError, TData>["queryKey"];
   }
-    )};
+) => {
+  return useQuery<GetHomePageQuery, TError, TData>({
+    queryKey:
+      variables === undefined ? ["GetHomePage"] : ["GetHomePage", variables],
+    queryFn: fetcher<GetHomePageQuery, GetHomePageQueryVariables>(
+      GetHomePageDocument
+    ).bind(null),
+    ...options,
+  });
+};
